bfe3ffbca06745461fda8253f3a8a7b49e8540a6
KD191996 <katyushadanilenko82@gmail.com>
2024-02-24 22:18:12 +0200

    finish hw7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+// Task 1
+
+const name = 'John';
+const admin = name;
+alert(admin);
+
+// Task 2
+
+const planet = 'Earth';
+console.log(planet);
+
+const user = 'Katya';
+console.log(user);
+
+// Task 3
+
+const num1 = "10";
+const num2= 10;
+
+console.log(typeof num1);
+console.log(typeof num2);
