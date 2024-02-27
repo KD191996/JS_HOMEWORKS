d3c33e5d1e59b0e202d47a7a4f007866a5616e03
KD191996 <katyushadanilenko82@gmail.com>
2024-02-27 11:29:57 +0200

    finish hw6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+let userFirstName = prompt('What is your name?');
+let userLastName= prompt('What is your last name?');
+let userSurname =prompt('What is your surname?');
+let userFullName = userFirstName + ' ' + userLastName + ' ' + userSurname;
+console.log(userFullName);
+
+
+let num = 12345;
+let array = [...num.toString()] .map(Number) .join(' ');
+console.log(array);
+
