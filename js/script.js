4a80b687a6e072d1e3fa74bc89d31aa0392c52cb
KD191996 <katyushadanilenko82@gmail.com>
2024-02-29 10:22:54 +0200

    change let on const hw8

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict'
 
-let userFirstName = prompt('What is your name?');
-let userLastName= prompt('What is your last name?');
-let userSurname =prompt('What is your surname?');
-let userFullName = userFirstName + ' ' + userLastName + ' ' + userSurname;
+const userFirstName = prompt('What is your name?');
+const userLastName= prompt('What is your last name?');
+const userSurname =prompt('What is your surname?');
+const userFullName = userFirstName + ' ' + userLastName + ' ' + userSurname;
 console.log(userFullName);
 
 
-let num = 12345;
-let array = [...num.toString()] .map(Number) .join(' ');
+const num = 12345;
+const array = [...num.toString()] .map(Number) .join(' ');
 console.log(array);
 
