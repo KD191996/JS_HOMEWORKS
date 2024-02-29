7f8510d306ffe6cadc9e3b73c5533e52c0b9ddf9
KD191996 <katyushadanilenko82@gmail.com>
2024-02-29 10:18:53 +0200

    finish hw6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict'
+
+// prompt('Скільки вам років?')
+
+const question = Number(prompt("Скільки вам років?", ''));
+const result = question + 10;
+const answer = ("Через 10 років вам буде");
+alert(answer + " " + result);
