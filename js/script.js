d9c0ce47d6d3a0931e0c6aa94b8c567a03aeaf50
KD191996 <katyushadanilenko82@gmail.com>
2024-03-01 21:39:21 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const operation = prompt('Яку математичну операцію ви хочете здійснити (+, -, *, /)?')
+const firstNumber = +prompt('Введіть перше число')
+const secondNumber = +prompt('Введіть друге число')
+
+
+if (operation) {
+    let result = null;
+
+    if (operation === '+'){
+        result = (firstNumber + secondNumber)
+    }
+
+    if (operation === '-') {
+        result = (firstNumber - secondNumber)
+    }
+
+    if (operation === '*') {
+        result = (firstNumber * secondNumber)
+    }
+
+    if (operation === '/'){
+        result = (firstNumber / secondNumber)
+    }
+    else{
+        result = ('Дані введені не вірно. Перевірте та спробуйте ще раз');
+    }
+    alert(result)
+}
+
