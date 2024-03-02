299539d6bbd9157bd16082c521fe6dd5dfcdcca5
KD191996 <katyushadanilenko82@gmail.com>
2024-03-02 16:35:40 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 'use strict'
+
+let firstNum = +prompt('Введіть перше число');
+let secondNum = +prompt('Введіть друге число');
+
+function sum () {
+
+    let result = firstNum + secondNum;
+    console.log('Результат:' + firstNum + '+' + secondNum + '=' + result);
+}
+sum(firstNum, secondNum);
+
+function sub () {
+
+    let result = firstNum - secondNum;
+    console.log('Результат:' + firstNum + '-' + secondNum + '=' + result);
+}
+sub(firstNum, secondNum);
+
+function div () {
+    if (secondNum === 0) {
+        console.log("Помилка: ділення на нуль");
+    return
+    }
+    let result = firstNum / secondNum;
+    console.log('Результат:' + firstNum + '/' + secondNum + '=' + result);
+
+}
+div(firstNum, secondNum);
+
+function mult () {
+
+    let result = firstNum * secondNum;
+    console.log('Результат:' + firstNum + '*' + secondNum + '=' + result);
+}
+mult(firstNum, secondNum);
+
+
+
+
