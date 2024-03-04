0a87ca4c1d10aa084425a6f9e8e010e7706c40d0
KD191996 <katyushadanilenko82@gmail.com>
2024-03-04 10:16:50 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 'use strict'
 
-let firstNum = +prompt('Введіть перше число');
-let secondNum = +prompt('Введіть друге число');
+const firstNum = +prompt('Введіть перше число');
+const secondNum = +prompt('Введіть друге число');
 
 function sum () {
 
-    let result = firstNum + secondNum;
+    const result = firstNum + secondNum;
     console.log('Результат:' + firstNum + '+' + secondNum + '=' + result);
 }
 sum(firstNum, secondNum);
 
 function sub () {
 
-    let result = firstNum - secondNum;
+    const result = firstNum - secondNum;
     console.log('Результат:' + firstNum + '-' + secondNum + '=' + result);
 }
 sub(firstNum, secondNum);
@@ -22,7 +22,7 @@ function div () {
         console.log("Помилка: ділення на нуль");
     return
     }
-    let result = firstNum / secondNum;
+    const result = firstNum / secondNum;
     console.log('Результат:' + firstNum + '/' + secondNum + '=' + result);
 
 }
@@ -30,7 +30,7 @@ div(firstNum, secondNum);
 
 function mult () {
 
-    let result = firstNum * secondNum;
+    const result = firstNum * secondNum;
     console.log('Результат:' + firstNum + '*' + secondNum + '=' + result);
 }
 mult(firstNum, secondNum);
