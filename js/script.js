0fe952f83475d744240375d5152b68d20de0ab62
KD191996 <katyushadanilenko82@gmail.com>
2024-03-06 16:20:43 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+let number1 = Number (prompt('Введіть перше число'));
+if (isNaN (number1)) {
+    alert('Введіть вірне значення для першого числа');
+}
+
+let number2 = Number (prompt('Введіть друге число'));
+if (isNaN(number2)) {
+    alert('Введіть вірне значення для другого числа');
+}
+
+let number3 = Number (prompt('Введіть третє число'));
+if (isNaN(Number(number3))) {
+    alert('Введіть вірне значення для третього числа');
+}
+
+alert( "Середнє арифметичне: " + (number1 + number2 + number3) / 3)
+
+
+console.log(typeof number1)
+console.log(typeof number2)
+console.log(typeof number3)
