bf53e7ffaa62b20eb2a6d2f84108504534a77c91
KD191996 <katyushadanilenko82@gmail.com>
2024-03-10 12:07:45 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const age = prompt('Введіть ваш вік');
+
+if (!isNaN(age)) {
+    if (age === '1'){
+        alert(age + ' ' + 'рік')
+    } else if (age > 1 && age < 5) {
+        alert(age + ' ' + 'роки')
+    } else {
+        alert(age + ' ' + 'років')
+    }
+} else {
+    alert('Введіть число')
+}
+
+
