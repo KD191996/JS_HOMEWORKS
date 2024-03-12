5530c1beee1dc6d58a468525ac765c3470cc274f
KD191996 <katyushadanilenko82@gmail.com>
2024-03-12 14:34:43 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (true) {
+    case numOrStr === null:
+        alert('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        alert('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        alert('number is Ba_NaN');
+        break;
+    default:
+        alert(`OK! ${numOrStr}`);
+}
