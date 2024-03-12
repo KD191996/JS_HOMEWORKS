ea40ae6f88d50edf18fe24f8499f9da0f7bd1e41
KD191996 <katyushadanilenko82@gmail.com>
2024-03-12 15:23:45 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,41 +1,62 @@
 'use strict'
 
 const year = prompt('Введіть рік вашого народження');
+const city = prompt('Введіть місто в якому ви живите');
+const sport = prompt('Введіть ваш улюблений вид спорту (футбол, гандбол, бокс)');
+
+let age = null
+
  if (year === null) {
      alert('Шкода, що ви не захотіли ввести ваш рік народження.')
  } else {
      const currentYear = new Date(). getFullYear();
-     const age = currentYear - year;
-     const result= ('Ваш вік:' + ' ' + age + '/n')
+     age = currentYear - year;
  }
 
-const city = prompt('Введіть місто в якому ви живите');
- let result = null
+let country = null;
+
 if (city === null) {
     alert('Шкода, що ви не захотіли ввести місто, в якому ви живете')
 } else {
     switch (city.toLowerCase()){
         case 'київ':
-            result = ('Ти живеш у столиці України') ;
+            country = ('Ти живеш у столиці України.') ;
             break;
         case 'вашингтон':
-            result = ('Ти живеш у столиці США');
+            country = ('Ти живеш у столиці США.');
             break;
         case 'лондон':
-            result = ('Ти живеш у столиці  Англії та Сполученого Королівства');
+            country = ('Ти живеш у столиці  Англії та Сполученого Королівства.');
             break;
         default:
-            result = ('Ти живеш у місті' + ' ' + city)
+            country = ('Ти живеш у місті' + ' ' + city + '.')
+    }
+}
+ let champ = null;
+
+if (sport === null) {
+    alert('Шкода, що ви не захотіли ввести ваш улюблений вид спорту')
+} else {
+    switch (sport) {
+        case 'футбол':
+            champ = 'Круто! Хочеш стати, як Мессі?';
+            break;
+        case 'гандбол':
+            champ = 'Круто! Хочеш стати, як Хансен?';
+            break;
+        case 'бокс':
+            champ = 'Круто! Хочеш стати, як Тайсон Фьюри?';
+            break
+        default:
+            champ = 'Вибери вид спорту з вищеперерахованих';
     }
 }
 
 
-alert(result)
-//
-// const sport = prompt('Введіть ваш улюблений вид спорту');
-// if (sport === null) {
-//     alert('Шкода, що ви не захотіли ввести ваш улюблений вид спорту')
-// }
+alert('Ваш вік:' + ' ' + age + ' ' + country + ' ' + champ)
+
+
+
 
 
 
