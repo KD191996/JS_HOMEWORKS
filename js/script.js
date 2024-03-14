ad432ab75c331b741ee831a203588e55d2ebc77e
KD191996 <katyushadanilenko82@gmail.com>
2024-03-14 16:40:52 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 'use strict'
 
+const array = [1, 2, 3, 4, 5];
+
+let summ = 0
+
+for (let a = 0; a < array.length; a++) {
+    summ += array[a]
+}
+
+console.log("Сума елементів масиву:", summ);
+
+// Даний масив із числами. За допомогою циклу знайдіть суму квадратів елементів цього масиву.
+
 const arr = [1, 2, 3, 4, 5];
 
 let sum = 0
 
 for (let a = 0; a < arr.length; a++) {
-    sum += arr[a]
+
+    sum += Math.pow(arr[a], 2)
 }
 
 console.log("Сума елементів масиву:", sum);
+
+
