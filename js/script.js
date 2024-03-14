9a3ed5e2373240e9b0ef7b197a9cc3bcde3246c2
KD191996 <katyushadanilenko82@gmail.com>
2024-03-14 21:17:29 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+// 1.
+for (let i = 20; i <= 30; i += 0.5) {
+    console.log(i);
+}
+
+// 2.
+const one_dollar = 27;
+
+for (let a = 10; a <= 100; a += 10) {
+    const uah = a * one_dollar
+    console.log( a + ' ' + 'долларів' + ' ' + '=' + uah + ' ' + 'гривень')
+}
+
+// 3.
