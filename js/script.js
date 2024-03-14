c6e9df825aa746f048881a923829f8fcfd441926
KD191996 <katyushadanilenko82@gmail.com>
2024-03-14 21:34:35 +0200

    init

--- js/script.js
+++ js/script.js
@@ -2,118 +2,119 @@
 
 
 // 1.
-// let a = '';
-//
-// for (let i = 10; i <= 20; i++) {
-//
-//     a += i;
-//
-//     if (i < 20) {
-//         a += ', ';
-//     }
-// }
-//
-// alert(a)
+let a = '';
+
+for (let i = 10; i <= 20; i++) {
+
+    a += i;
+
+    if (i < 20) {
+        a += ', ';
+    }
+}
+
+alert(a)
 
 // 2.
-// for (let b = 10; b <= 20; b++ ) {
-//     const square = b * 2;
-//     console.log('квадрат числа' + ' ' + b + ' ' + 'дорівнює' + ' ' + square)
-// }
+for (let b = 10; b <= 20; b++ ) {
+    const square = b * 2;
+    console.log('квадрат числа' + ' ' + b + ' ' + 'дорівнює' + ' ' + square)
+}
 
 // 3.
-// for (let a = 0;  a <= 10; a++) {
-//     const mult = 7 * a;
-//     console.log(mult)
-// }
+for (let c = 0;  c <= 10; c++) {
+    const mult = 7 * c;
+    console.log(mult)
+}
 
 // 4.
-// let sum = 0;
+let sum = 0;
 
-// for (let i = 1; i <= 15; i++) {
-//     sum += i;
-// }
+for (let f = 1; f <= 15; f++) {
+    sum += f;
+}
 
-// console.log('Сума всіх цілих чисел від 1 до 15:', sum);
+console.log('Сума всіх цілих чисел від 1 до 15:', sum);
 
 // 5.
-// let mult = 1;
-//
-// for (let a = 15; a <= 35; a++) {
-//     mult *= a;
-// }
-// console.log('Добуток всіх цілих чисел від 15 до 35:', mult)
+let multi = 1;
+
+for (let h = 15; h <= 35; h++) {
+    multi *= h;
+}
+console.log('Добуток всіх цілих чисел від 15 до 35:', multi)
+
 
 // 6.
-// let sum = 0;
-//
-// for (let a = 1; a <= 500; a++) {
-//     sum += a ;
-// }
-// let average = sum / 500
-//
-// console.log('середнє арифметичне всіх цілих чисел від 1 до 500:' + ' ' + average)
+let summ = 0;
+
+for (let t = 1; t <= 500; t++) {
+    summ += t ;
+}
+let average = summ / 500
+
+console.log('середнє арифметичне всіх цілих чисел від 1 до 500:' + ' ' + average)
 
 // 7.
-// let sum = 0;
-//
-// for (let a = 30; a <= 80; a++){
-//
-//     if (a % 2 === 0) {
-//         sum += a;
-//     }
-// }
-// console.log('суму лише парних чисел в діапазоні від 30 до 80:' + sum)
+let amount = 0;
+
+for (let q = 30; q <= 80; q++){
+
+    if (q % 2 === 0) {
+        amount += q;
+    }
+}
+console.log('суму лише парних чисел в діапазоні від 30 до 80:' + sum)
 
 // 8.
-// for (let a = 100; a <= 200; a++) {
-//     if (a % 3 === 0) {
-//         console.log(a);
-//     }
-// }
+for (let r = 100; r <= 200; r++) {
+    if (r % 3 === 0) {
+        console.log(r);
+    }
+}
 
 // 9.
-// let number = 18;
-// let b = null;
-//
-// for (let a = 1; a <= number; a++){
-//     b = a;
-//     if (number % a === 0) {
-//         console.log (b)
-//     }
-// }
+let number = 18;
+let n = null;
+
+for (let m = 1; m <= number; m++){
+    n = m;
+    if (number % m === 0) {
+        console.log (n)
+    }
+}
 
 // 10.
-// let number = 18;
-// let a = null;
-//
-// for (let b = 1; b <= number; b++){
-//     if (number % b === 0 && b % 2 === 0) {
-//         a = b
-//         console.log(a)
-//     }
-// }
+let numb = 18;
+let w = null;
+
+for (let s = 1; s <= number; s++){
+    if (numb % s === 0 && s % 2 === 0) {
+        w = s
+        console.log(w)
+    }
+}
 
 // 11.
-// let number = 18;
-// let sum = 0;
-//
-//
-// for (let b = 1; b <= number; b++){
-//     if (number % b === 0 && b % 2 === 0) {
-//         sum += b;
-//     }
-// }
-// console.log('сумма парних дільников:' + sum)
+let num = 18;
+let sym = 0;
+
+
+for (let l = 1; l <= num; l++){
+    if (num % l === 0 && l % 2 === 0) {
+        sym += l;
+    }
+}
+console.log('сумма парних дільников:' + sym)
 
 // 12.
 
-for (let a = 1; a <= 10; a++) {
+for (let y = 1; y <= 10; y++) {
 
-    for (let b = 1; b <= 10; b++){
-       let sum = b * a;
+    for (let z = 1; z <= 10; z++){
+       let amoun = z * y;
 
-        console.log( a + '*' + b + '=' + sum + '\n')
+        console.log( y + '*' + z + '=' + amoun + '\n')
     }
 
 }
