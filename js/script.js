82b658b65c3a2a4b6e032addac3cd8949adc3ad6
KD191996 <katyushadanilenko82@gmail.com>
2024-03-16 20:14:13 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const num = +prompt('Введіть число');
+const degree = +prompt('Введіть ступінь до якого необхідно піднести число');
+
+function foo (num, degree = 1) {
+    if (isNaN(num) && isNaN(degree)){
+        return 'помилка. Обидва аргументи мають бути числами.'
+    }
+
+    return 'Число' + ' ' + num + ' ' + 'в' + ' ' + degree + ' ' + 'ступені' + '='+ Math.pow (num, degree)
+}
+
+const result = foo (num, degree);
+alert(result)
