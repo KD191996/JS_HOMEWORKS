11c79836ebb2519f49af02b4c209a2cc12c21734
KD191996 <katyushadanilenko82@gmail.com>
2024-03-16 20:52:38 +0200

    init

--- Readme.md
+++ Readme.md
@@ -1,2 +1,12 @@
-###HOMEWORK 4
+###HOMEWORK 27
+
+Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із 
+набору characters довжиною length. span>
+
+Наприклад:
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
 
--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length, characters) {
+    let key = '';
+
+    for (let a = 0; a <= length; a++) {
+        const randomIndex = Math.floor(Math.random() * characters.length)
+        key += characters[randomIndex]
+    }
+
+    return key;
+}
+
+const key = generateKey(16, characters)
+
+
+alert(key)
