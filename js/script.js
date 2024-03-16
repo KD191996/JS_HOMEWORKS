cf71815592b18e4b377d33e14a836225b5eef284
KD191996 <katyushadanilenko82@gmail.com>
2024-03-16 19:23:08 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const num = +prompt('Введіть число');
+
+if (num < 1) {
+    console.log("NaN");
+} else if (num === 1) {
+    console.log ('Мінімальний дільник числа' + ' ' + num + '-' + num)
+}else {
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0){
+            console.log('Мінімальний дільник числа' + ' ' + num + '-' + i)
+            break
+        }
+    }
+}
+
