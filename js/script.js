99111ea12266ad86c130895c90d36fc9c38d4938
KD191996 <katyushadanilenko82@gmail.com>
2024-03-17 18:44:06 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const arr = [1, 2, 3, 4, -1, -2, -3, -4];
+
+function foo (arr) {
+    const positivArr = [];
+
+    if (arr.length === 0) {
+        return 'Масив порожній'
+    }
+
+    for (let a = 0; a < arr.length; a++){
+        if (arr[a] > 0) {
+            positivArr.push(arr[a]);
+        }
+    }
+
+    if (positivArr.length > 0) {
+        return positivArr;
+    } else {
+        return null;
+    }
+}
+
+const result = foo (arr)
+alert(result)
