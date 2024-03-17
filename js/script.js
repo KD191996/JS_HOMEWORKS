1a34cced41c9ffa646f71b0693b0fdecc57def0e
KD191996 <katyushadanilenko82@gmail.com>
2024-03-17 21:06:52 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 'use strict'
+
+const string = prompt('Введіть рядок');
+const lenght = +prompt(' Введіть число, яке буде визначати кількість символів');
+const symbol = prompt('Введіть символ для доповнення рядка')
+const right = prompt('Символ додати справа?(так/ні)') === 'так'
+
+
+function padString (string, lenght, symbol, right = true) {
+     if (string === undefined) {
+         return 'Помилка. Відсутнє значення в рядку'
+     }
+
+     if (lenght === undefined) {
+         return 'Помилка. Відсутнє число, яке визначає довжину рядка'
+     }
+
+    if (string.length >= lenght) {
+        return string.substring(0 , lenght);
+    }
+
+    if (symbol.length !== 1) {
+        return 'Помилка. Символ повинен бути довжиною в 1 символ.';
+    }
+
+    const differLenght = lenght - string.length;
+    const newString = symbol.repeat(differLenght);
+
+    if (right) {
+        return string + newString;
+    } else {
+        return newString + string;
+    }
+}
+
+const result = padString(string, lenght, symbol, right)
+
+alert(result)
+
+
+
