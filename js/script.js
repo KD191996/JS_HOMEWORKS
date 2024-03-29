a3b86938bd09df313f3ad4f440e0400ebedc347e
KD191996 <katyushadanilenko82@gmail.com>
2024-03-29 11:23:50 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -88,3 +88,29 @@ for (let i = arr.length - 1; i >= 0; i--) {
 }
 
 console.log(reversedArray);
+
+// 10.
+arr = [[1, 2, 3], [4, 5], [6]];
+sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        sum += arr[i][j];
+    }
+}
+
+console.log(sum);
+
+// 11.
+arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        for (let k = 0; k < arr[i][j].length; k++) {
+            sum += arr[i][j][k];
+        }
+    }
+}
+
+console.log(sum);
