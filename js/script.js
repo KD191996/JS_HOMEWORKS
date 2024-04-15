6c84d9a3a07930f3eaf51abc74cb078de5c4a806
KD191996 <katyushadanilenko82@gmail.com>
2024-04-15 15:57:54 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ function getFactorial(n) {
 
 console.log(getFactorial(5));
 
+const getFactorial1 = (n) => (n === 0 || n === 1 ? 1 : n * getFactorial(n - 1));
+console.log(getFactorial1(5));
 // Task #2
 function pow(num, degree) {
   if (degree === 0) {
@@ -19,6 +21,15 @@ function pow(num, degree) {
 
 console.log(pow(3, 4));
 
+const pow1 = (num, degree) => {
+  if (degree === 0) {
+    return 1;
+  }
+  return num * pow1(num, degree - 1);
+};
+
+console.log(pow1(3, 4));
+
 // Task #3
 function recursiveSum(a, b) {
   if (b === 0) {
@@ -26,5 +37,14 @@ function recursiveSum(a, b) {
   }
   return recursiveSum(a + 1, b - 1);
 }
+
+console.log(recursiveSum(4, 5));
+
+const recursiveSum1 = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return recursiveSum1(a + 1, b - 1);
+};
 
 console.log(recursiveSum(4, 5));
