de01192d3242660b9abfa5f07267fb22f80f6ed7
KD191996 <katyushadanilenko82@gmail.com>
2024-04-16 20:29:37 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-'use strict'
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13],
+];
 
+const flat = function (arr) {
+  if (arguments.length !== 1) {
+    console.error(
+      "Function accepts only 1 argument, too much arguments provided",
+    );
+  }
+  const result = [];
 
+  const flatten = (array) => {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < array.length; i++) {
+      if (!Array.isArray(array[i])) {
+        result.push(array[i]);
+      } else {
+        flatten(array[i]);
+      }
+    }
+  };
+  flatten(arr);
+  return result;
+};
+
+console.log(flat(complexArray));
