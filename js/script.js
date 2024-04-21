bc3194b8c67aea1a14c99f791fb8aa735bb9c7ac
KD191996 <katyushadanilenko82@gmail.com>
2024-04-21 12:08:44 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-'use strict'
+/* eslint-disable quotes */
+function myApply(func, context, args) {
+  if (typeof func !== "function") return "Перший аргумент має бути функцією";
 
+  let newContext = null;
+  if (!context) {
+    newContext = window;
+  } else {
+    newContext = context;
+  }
 
+  let result = null;
+  if (Array.isArray(args)) {
+    result = args;
+  } else if (typeof args === "object" && args !== null) {
+    result = [];
+    const keys = Object.keys(args);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      result.push(args[key]);
+    }
+  }
+  const boundFunc = func.bind(newContext);
+  return boundFunc(...result);
+}
+
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+};
+function fullName(city, country) {
+  return `My name is ${this.firstName} ${this.lastName}, I am from ${city},${country}`;
+}
+
+const about = myApply(fullName, person1, ["Odesa", "Ukraine"]);
+console.log(about);
