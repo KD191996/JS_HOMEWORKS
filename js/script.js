01115ad0215a9896cd1a2f229cd58c8d355ba67f
KD191996 <katyushadanilenko82@gmail.com>
2024-04-24 16:55:23 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-'use strict'
+(function () {
+  const call = function (func, ctx, arg) {
+    ctx.func = func;
+    ctx.func(arg);
+    delete ctx.func;
+  };
 
+  const bind = function (bFunc, bCtx, bArrayOfArg) {
+    return function () {
+      return call(bFunc, bCtx, bArrayOfArg);
+    };
+  };
 
+  const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+  };
+
+  const display = function () {
+    console.log(this.lastName);
+  };
+
+  const b = bind(display, person1);
+  b();
+})();
