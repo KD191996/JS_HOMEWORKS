78a5e62be3a85265caf8492cc4e6861cefb18440
KD191996 <katyushadanilenko82@gmail.com>
2024-04-25 12:25:19 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-'use strict'
+(() => {
+  const newSum = function () {
+    let sum = 0;
 
+    return function (number) {
+      sum += number;
+      return sum;
+    };
+  };
 
+  const sumi = newSum();
+
+  console.log(sumi(3));
+  console.log(sumi(5));
+  console.log(sumi(20));
+})();
