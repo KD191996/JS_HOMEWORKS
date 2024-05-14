03d5bfd290ce48d6ef3dd60046c17f5287a9e56a
KD191996 <katyushadanilenko82@gmail.com>
2024-05-14 10:53:39 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-'use strict'
+(function () {
+  function generateList(arr) {
+    const ulElem = document.createElement("ul");
 
+    for (const element of arr) {
+      const liElem = document.createElement("li");
+      if (Array.isArray(element)) {
+        liElem.append(generateList(element));
+      } else {
+        liElem.textContent = element;
+      }
+      ulElem.append(liElem);
+    }
+    return ulElem;
+  }
 
+  const list = generateList([1, 2, [2.1, 2.2], 3, [3.1, 3.2], 4]);
+  document.body.append(list);
+})();
