b1c0b19c3fe5e3c8fb57bf87067a2d2c1ffac911
KD191996 <katyushadanilenko82@gmail.com>
2024-06-08 12:11:36 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-'use strict'
+(function () {
+  const arr = [1, 2, 3, 4, 5];
 
+  const iterator = (data) => {
+    const myArr = Array.from(data);
+    let index = 0;
 
+    return {
+      next() {
+        let done = myArr.length <= index ? true : false;
+        const value = !done ? myArr[index] : undefined;
+
+        const result = {
+          value,
+          done,
+        };
+
+        index += 1;
+
+        return result;
+      },
+    };
+  };
+
+  const myIterator = iterator(arr);
+
+  console.log(myIterator.next());
+  console.log(myIterator.next());
+  console.log(myIterator.next());
+  console.log(myIterator.next());
+  console.log(myIterator.next());
+  console.log(myIterator.next());
+})();
